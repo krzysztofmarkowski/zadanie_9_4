aace46d5161e10d8114d580fbe9b75eb7d4637ef
Krzysztof Markowski <markowski@gdansk.home.pl>
2017-07-06 22:04:28 +0200

    For loop and while...do... together

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,14 @@ var star = "";
 
 function drawTree(number) {
     for (var i = 0; i < number; i++) {
+        var space = "";
+        var k = number - i;
+        do {
+            space += " ";
+            k--;
+        } while (k > 0);
         star += '*';
-        console.log(star);
+        console.log(space + star + star);
     }
 }
 
