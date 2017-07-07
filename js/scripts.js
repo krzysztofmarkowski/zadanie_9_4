bbada1a644965a0fedbc1675c9b8e45b6d8cb7af
Krzysztof Markowski <markowski@gdansk.home.pl>
2017-07-08 01:35:38 +0200

    Adding atribut lang in html and better look of the tree

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
 var number;
-var star = "";
+var star = "*";
 
 function drawTree(number) {
-    for (var i = 0; i < number; i++) {
-        var space = "";
-        var k = number - i;
-        do {
-            space += " ";
-            k--;
-        } while (k > 0);
-        star += '*';
-        console.log(space + star + star);
-    }
+  for (var i = 0; i < number; i++) {
+    var space = "";
+    var k = number - i;
+    do {
+        space += " ";
+        k--;
+    } while (k > 0);
+    console.log(space + star);
+    star += '**';
+  }
 }
-
 drawTree(7);
